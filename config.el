5c5ed09cd7bcee0fa4b23370936150da3cb0d913
sobin <sobingeneral@gmail.com>
2023-12-28 20:27:56 +0700

    update config

--- config.el
+++ config.el
@@ -33,11 +33,16 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 ;; (setq doom-theme 'doom-one)
-(setq doom-theme 'doom-henna)
+;; (setq doom-theme 'doom-henna)
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-(setq doom-font (font-spec :family "JetBrainsMono NF" :size 16))
+(setq
+ doom-theme 'doom-henna
+ display-line-numbers-type t
+ doom-font (font-spec :family "JetBrainsMono NF" :size 16)
+)
+
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
@@ -45,7 +50,10 @@
 ;; PATH SETTINGS FOR WINDOWS
 (when (eq system-type 'windows-nt)  ; windows
         (setenv "PATH" (concat (getenv "PATH") ";C:\\Users\\tarny\\.cargo\\bin"))
-        (setenv "PATH" (concat (getenv "PATH") ";C:\\MinGW\\bin")))
+        (setenv "PATH" (concat (getenv "PATH") ";C:\\MinGW\\bin"))
+        (setq projectile-project-search-path '("E:\\Programming\\"))
+)
+
 
 ;; Open doom emacs start in maximized window
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
