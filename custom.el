3d4d5503acea9ce010961828125727c1c2106073
sobin <Sobingeneral@gmail.com>
2024-01-02 01:40:41 +0700

    feat: naysayer custom.el

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("375a3611e04b912ff797dc11ddb219043f0a058f5449b50fb815d3bbf6697d8e" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
